eed2954d4289384b90798a930613810ce6ed3ef3
Hadi Azarnasab <hazard7546@gmail.com>
2025-03-13 00:09:10 +0330

    Add isModern prop to PoemPage for conditional rendering

--- src/app/poem/[id]/page.tsx
+++ src/app/poem/[id]/page.tsx
@@ -83,6 +83,7 @@ export default function PoemPage() {
                 onPrevious={() => { }}
                 isFirst={true}
                 isLast={true}
+                isModern={false}
             />
         </main>
     ) : null;
